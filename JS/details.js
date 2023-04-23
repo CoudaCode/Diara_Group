c241b920233af828f7e2db2d92ba83cb4b446a70
Diarassouba Mamadou <couda.dm@gmail.com>
2023-04-23 17:30:33 +0000

    diar

--- JS/details.js
+++ JS/details.js
@@ -1,38 +1,35 @@
-
-
-(() =>{
-   console.log('test')
+window.addEventListener('DOMContentLoaded', ()=>{
    const openNavMenu = document.querySelector(".open-nav-menu"),
    closeNavMenu = document.querySelector(".close-nav-menu"),
    navMenu = document.querySelector(".nav-menu"),
    menuOverlay = document.querySelector(".menu-overlay"),
    mediaSize = 991;
- 
+   
    openNavMenu.addEventListener("click", toggleNav);
    closeNavMenu.addEventListener("click", toggleNav);
    // close the navMenu by clicking outside
    menuOverlay.addEventListener("click", toggleNav);
- 
+   
    function toggleNav() {
-      navMenu.classList.toggle("open");
-      menuOverlay.classList.toggle("active");
-      document.body.classList.toggle("hidden-scrolling");
+     navMenu.classList.toggle("open");
+     menuOverlay.classList.toggle("active");
+     document.body.classList.toggle("hidden-scrolling");
    }
- 
+   
    navMenu.addEventListener("click", (event) =>{
        if(event.target.hasAttribute("data-toggle") && 
-          window.innerWidth <= mediaSize){
-          // prevent default anchor click behavior
-          event.preventDefault();
-          const menuItemHasChildren = event.target.parentElement;
+         window.innerWidth <= mediaSize){
+         // prevent default anchor click behavior
+         event.preventDefault();
+         const menuItemHasChildren = event.target.parentElement;
          // if menuItemHasChildren is already expanded, collapse it
          if(menuItemHasChildren.classList.contains("active")){
-            collapseSubMenu();
+           collapseSubMenu();
          }
          else{
            // collapse existing expanded menuItemHasChildren
            if(navMenu.querySelector(".menu-item-has-children.active")){
-            collapseSubMenu();
+           collapseSubMenu();
            }
            // expand new menuItemHasChildren
            menuItemHasChildren.classList.add("active");
@@ -42,28 +39,27 @@
        }
    });
    function collapseSubMenu(){
-      navMenu.querySelector(".menu-item-has-children.active .sub-menu")
-      .removeAttribute("style");
-      navMenu.querySelector(".menu-item-has-children.active")
-      .classList.remove("active");
+     navMenu.querySelector(".menu-item-has-children.active .sub-menu")
+     .removeAttribute("style");
+     navMenu.querySelector(".menu-item-has-children.active")
+     .classList.remove("active");
    }
    function resizeFix(){
-       // if navMenu is open ,close it
-       if(navMenu.classList.contains("open")){
-          toggleNav();
-       }
-       // if menuItemHasChildren is expanded , collapse it
-       if(navMenu.querySelector(".menu-item-has-children.active")){
-            collapseSubMenu();
+      // if navMenu is open ,close it
+      if(navMenu.classList.contains("open")){
+        toggleNav();
+      }
+      // if menuItemHasChildren is expanded , collapse it
+      if(navMenu.querySelector(".menu-item-has-children.active")){
+           collapseSubMenu();
       }
    }
- 
+   
    window.addEventListener("resize", function(){
       if(this.innerWidth > mediaSize){
-         resizeFix();
+        resizeFix();
       }
    });
  
- })();
- 
+ })
  
